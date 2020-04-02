9110aa11116e6ebae023bcaef8457780480532e9
Sakthi Murugan Subramani <sms207@gmail.com>
2020-04-02 11:53:08 +0530

    Tic-tac-toe

---
+++ precache-manifest.896a22e8b863a7176e48be35f2f30c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f453bc825fe051ce0e042a79d86ed1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40144b07008c2aaaaae5",
+    "url": "/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "77d181ab02e6a634f0d4",
+    "url": "/static/js/2.3753c32d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3753c32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40144b07008c2aaaaae5",
+    "url": "/static/js/main.35069495.chunk.js"
+  },
+  {
+    "revision": "f6633bf07a047e0bb61c",
+    "url": "/static/js/runtime-main.23127ecf.js"
+  }
+]);
